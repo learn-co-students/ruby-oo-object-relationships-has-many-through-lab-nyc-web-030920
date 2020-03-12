d306c9f0bcbcdae3ca3fffc21655ee61e8073aa2
MisterC1ean <robert_keller44@icloud.com>
2020-03-11 21:04:39 -0400

    working

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+        # given a name and genre, creates a new song associated with that artist
+    end
+
+    def genres
+       songs.select do |song|
+            song.genre
+       end
+    #  has many genres, through songs  
+    # expecting artist.genres
+    # jz.genres => rap
+    end
+end
